1b1894e1217d83f95cb285c6f74a66f18d18b879
EC2 Default User <ec2-user@ip-172-31-45-100.ap-southeast-2.compute.internal>
2023-10-31 14:50:44 +0000

    じゃんけん　インデント修正と任意の数字入力以外の処理

--- janken.rb
+++ janken.rb
@@ -1,87 +1,92 @@
 def janken
   puts"じゃんけん..."
-  puts"0(グー)1(チョキ)2(パー)3(戦わない)"
+  puts"1(グー)2(チョキ)3(パー)4(戦わない)"
+  puts"-------------------------"
   player_hand = gets.to_i
+  player_hand -= 1
   program_hand = rand(3)
- 
-  if player_hand > 3
- 	  puts "入力された値が無効です"
- 	  puts"-------------------------"
-  return true
-	
-  elsif player_hand == 3
+  
+  if player_hand == 3
     puts"出さなかったので負けです"
   return false
- 
-  end
- 
-  jankens=["グー","チョキ","パー"]
-  puts"あなた:#{jankens[player_hand]}を出しました"
-  puts"相　手:#{jankens[program_hand]}を出しました"
-  puts"-------------------------"
-
-  if player_hand == program_hand
-	  puts "あいこ"
-	  puts"-------------------------"
+  
+  elsif player_hand < 0 || player_hand > 3
+    puts"入力した値が無効です"
+    puts"-------------------------"
   return true
-	
-	
-  elsif (player_hand == 0 && program_hand ==1) ||(player_hand == 1 && program_hand ==2) || (player_hand == 2 && program_hand ==0) #じゃんけん勝ち
-	  puts "あっちむいて〜"
-	  puts"0(右)1(左)2(上)3(下)"
-	  player_hand2 = gets.to_i
-	  program_hand2 = rand(4)
-	
-  	if player_hand2 > 3
-	    puts "入力した値が無効なのであなたの負けです"
-	  return false
-	  
-	  end
-	
-	  attimuites=["右","左","上","下"]
-	  puts"-------------------------"
-    puts"ホイ!"
-    puts"あなた:#{attimuites[player_hand2]}"
-    puts"相　手:#{attimuites[program_hand2]}"
-    puts"-------------------------"
   
-    if player_hand2 == program_hand2
-	    puts "あなたの勝ちです"
-	  return false
-	 
-	  else
-	  return true
-	  end
-	
-	
-  else #じゃんけん負け
-    puts "あっちむいて〜"
-    puts"0(右)1(左)2(上)3(下)"
-    player_hand2 = gets.to_i
-    program_hand2 = rand(4)
+  end
+  
+ 
+    jankens=["グー","チョキ","パー"]
+    puts"あなた:#{jankens[player_hand]}を出しました"
+    puts"相　手:#{jankens[program_hand]}を出しました"
+    puts"-------------------------"
 
-    if player_hand2 > 3
-      puts "入力した値が無効なのであなたの負けです"
-    return false
-    end
+    if player_hand == program_hand
+	    puts "あいこ"
+	    puts"-------------------------"
+    return true
 	
-    attimuites=["右","左","上","下"]
-    puts"-------------------------"
-    puts"ホイ!"
-    puts"相　手:#{attimuites[program_hand2]}"
-    puts"あなた:#{attimuites[player_hand2]}"
-    puts"-------------------------"
-  
-    if player_hand2 == program_hand2
-	    puts "あなたの負けです"
-	  return false
-	 
-	  else
-	  return true
-	 
-	  end
+	
+    elsif (player_hand == 0 && program_hand ==1) ||(player_hand == 1 && program_hand ==2) || (player_hand == 2 && program_hand ==0) #じゃんけん勝ち
+	    puts "あっちむいて〜"
+	    puts"1(右)2(左)3(上)4(下)"
+	    puts"-------------------------"
+	    player_hand2 = gets.to_i
+	    player_hand2 -= 1
+	    program_hand2 = rand(4)
+	
+  	  if player_hand2 < 0 || player_hand2 > 3
+	      puts "入力した値が無効なのであなたの負けです"
+	    return false
 	  
-  end 
+	    end
+	
+	    attimuites=["右","左","上","下"]
+      puts"ホイ!"
+      puts"あなた:#{attimuites[player_hand2]}"
+      puts"相　手:#{attimuites[program_hand2]}"
+      puts"-------------------------"
+  
+      if player_hand2 == program_hand2
+	      puts "あなたの勝ちです"
+	    return false
+	 
+	    else
+	    return true
+	    end
+	
+	
+    else #じゃんけん負け
+      puts "あっちむいて〜"
+      puts"1(右)2(左)3(上)4(下)"
+      puts"-------------------------"
+      player_hand2 = gets.to_i 
+      player_hand2 -= 1
+      program_hand2 = rand(4)
+
+      if player_hand2 < 0 || player_hand2 > 3
+        puts "入力した値が無効なのであなたの負けです"
+      return false
+      end
+	
+      attimuites=["右","左","上","下"]
+      puts"ホイ!"
+      puts"相　手:#{attimuites[program_hand2]}"
+      puts"あなた:#{attimuites[player_hand2]}"
+      puts"-------------------------"
+  
+      if player_hand2 == program_hand2
+	      puts "あなたの負けです"
+	    return false
+	 
+	    else
+	    return true
+	 
+	    end
+	  
+    end
   
 end
 	 
