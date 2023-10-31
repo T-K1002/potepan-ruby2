5f41c452f33bdb1d188bbe646d4dc19742377af1
EC2 Default User <ec2-user@ip-172-31-45-100.ap-southeast-2.compute.internal>
2023-10-31 06:20:44 +0000

    じゃんけんアプリ１回目

---
+++ janken.rb
@@ -0,0 +1,97 @@
+def janken
+ puts"じゃんけん..."
+ puts"0(グー)1(チョキ)2(パー)3(戦わない)"
+ player_hand = gets.to_i
+ program_hand = rand(3)
+ 
+ if player_hand > 3
+	puts "入力された値が無効です"
+	puts"-------------------------"
+	return true
+	
+ elsif player_hand == 3
+ puts"出さなかったので負けです"
+ return false
+ 
+ end
+ 
+ jankens=["グー","チョキ","パー"]
+ puts"あなた:#{jankens[player_hand]}を出しました"
+ puts"相　手:#{jankens[program_hand]}を出しました"
+ puts"-------------------------"
+
+ if player_hand == program_hand
+	puts "あいこ"
+	puts"-------------------------"
+ return true
+	
+	
+	elsif (player_hand == 0 && program_hand ==1) ||(player_hand == 1 && program_hand ==2) || (player_hand == 2 && program_hand ==0) #じゃんけん勝ち
+	def attimuite
+	puts "あっちむいて〜"
+	puts"0(右)1(左)2(上)3(下)"
+	player_hand2 = gets.to_i
+	program_hand2 = rand(4)
+	attimuites=["右","左","上","下"]
+	puts"-------------------------"
+ puts"ホイ!"
+ puts"あなた:#{attimuites[player_hand2]}"
+ puts"相　手:#{attimuites[program_hand2]}"
+ puts"-------------------------"
+  
+  if player_hand2 == program_hand2
+	 puts "あなたの勝ちです"
+	 return false
+	 
+	 else
+	 return true
+	 
+	 end
+	end
+	
+	next_game2 = true
+ while next_game2
+ next_game2 = attimuite
+ end
+return false
+	
+else #じゃんけん負け
+def attimuite
+puts "あっちむいて〜"
+puts"0(右)1(左)2(上)3(下)"
+player_hand2 = gets.to_i
+program_hand2 = rand(4)
+attimuites=["右","左","上","下"]
+puts"-------------------------"
+puts"ホイ!"
+puts"相　手:#{attimuites[program_hand2]}"
+puts"あなた:#{attimuites[player_hand2]}"
+puts"-------------------------"
+  
+ if player_hand2 == program_hand2
+	puts "あなたの負けです"
+	return false
+	 
+	else
+	return true
+	 
+	end
+end 
+	 
+	next_game2 = true
+ while next_game2
+ next_game2 = attimuite
+ end
+
+return false 
+end
+ 
+ 
+end
+ 
+next_game = true
+while next_game
+next_game = janken
+end
+  
+	
